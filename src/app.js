f7999dd56170de9854fdecfdc835eba50ade3779
Hanna Vretby <hr222sy@student.lnu.se>
2026-02-19 11:07:03 +0100

    feat: add errorHandler middleware to app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
+import errorHandler from './middleware/errorHandler.js'
 
 const app = express()
 
 app.use(helmet())
 app.use(cors())
 app.use(express.json())
+app.use(errorHandler)
 
 export default app
