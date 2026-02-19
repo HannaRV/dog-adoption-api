15691c66648c76219c0cbbb49dc209a164bc91db
Hanna Vretby <hr222sy@student.lnu.se>
2026-02-19 11:30:08 +0100

    feat: add TravelService

---
+++ src/services/TravelService.js
@@ -0,0 +1,26 @@
+import TravelRepository from '../repositories/TravelRepository.js'
+import NotFoundError from '../utils/errors/NotFoundError.js'
+
+export default class TravelService {
+  #travelRepository
+
+  constructor () {
+    this.#travelRepository = new TravelRepository()
+  }
+
+  #throwIfTravelNotFound (travelRecord) {
+    if (!travelRecord) {
+      throw new NotFoundError('Travel record not found')
+    }
+  }
+
+  async getAllTravelRecords ({ page = 1, travelsPerPage = 20 }) {
+    return this.#travelRepository.findAll({ page, travelsPerPage })
+  }
+
+  async getTravelRecordByPetfinderId (petfinderId) {
+    const travelRecord = await this.#travelRepository.findByPetfinderId(petfinderId)
+    this.#throwIfTravelNotFound(travelRecord)
+    return travelRecord
+  }
+}
