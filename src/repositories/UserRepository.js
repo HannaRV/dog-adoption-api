439bb5658bfdf27cfc06ee6098535f3432f36ef1
Hanna Vretby <hr222sy@student.lnu.se>
2026-02-20 15:50:35 +0100

    refactor: add JSDoc header, dependency injection and lean to UserRepository

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -1,12 +1,25 @@
+/**
+ * @file Repository for user data access.
+ * @module src/repositories/UserRepository.js
+ * @author Hanna Rubio Vretby <hr222sy@student.lnu.se>
+ * @version 1.0.0
+ */
+
 import User from '../models/User.js'
 
 export default class UserRepository {
+  #model
+
+  constructor (model = User) {
+    this.#model = model
+  }
+
   async findByEmail (email) {
-    return User.findOne({ email })
+    return this.#model.findOne({ email }).lean().exec()
   }
 
   async create (userData) {
-    const user = new User(userData)
+    const user = new this.#model(userData)
     return user.save()
   }
 }
