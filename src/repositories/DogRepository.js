20ca993fbd38a6d1374685e94ccd3b53c7181319
Hanna Vretby <hr222sy@student.lnu.se>
2026-02-20 11:52:59 +0100

    refactor: add dependency injection and lean to DogRepository

--- src/repositories/DogRepository.js
+++ src/repositories/DogRepository.js
@@ -1,33 +1,39 @@
 import Dog from '../models/Dog.js'
 
 export default class DogRepository {
+  #model
+
+  constructor (model = Dog) {
+    this.#model = model
+  }
+
   #calculateDogsToSkip (page, dogsPerPage) {
     return (page - 1) * dogsPerPage
   }
 
   async findAll ({ filter = {}, page = 1, dogsPerPage = 20 }) {
     const dogsToSkip = this.#calculateDogsToSkip(page, dogsPerPage)
-    const dogs = await Dog.find(filter).skip(dogsToSkip).limit(dogsPerPage)
-    const totalDogs = await Dog.countDocuments(filter)
+    const dogs = await this.#model.find(filter).skip(dogsToSkip).limit(dogsPerPage).lean({ virtuals: Boolean(this.#model.schema.virtuals) }).exec()
+    const totalDogs = await this.#model.countDocuments(filter)
     const totalPages = Math.ceil(totalDogs / dogsPerPage)
 
     return { dogs, totalDogs, totalPages, page, dogsPerPage }
   }
 
   async findById (id) {
-    return Dog.findById(id)
+    return this.#model.findById(id).lean({ virtuals: Boolean(this.#model.schema.virtuals) }).exec()
   }
 
   async create (dogData) {
-    const dog = new Dog(dogData)
+    const dog = new this.#model(dogData)
     return dog.save()
   }
 
   async update (id, dogData) {
-    return Dog.findByIdAndUpdate(id, dogData, { new: true })
+    return this.#model.findByIdAndUpdate(id, dogData, { new: true }).lean({ virtuals: Boolean(this.#model.schema.virtuals) }).exec()
   }
 
   async remove (id) {
-    return Dog.findByIdAndDelete(id)
+    return this.#model.findByIdAndDelete(id).lean({ virtuals: Boolean(this.#model.schema.virtuals) }).exec()
   }
 }
