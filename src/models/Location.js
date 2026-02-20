6e4cea2ac6189c910158c8f1e8aa6db6cb8ee140
Hanna Vretby <hr222sy@student.lnu.se>
2026-02-20 15:42:57 +0100

    refactor: add JSDoc header and required to location in Location model

--- src/models/Location.js
+++ src/models/Location.js
@@ -1,10 +1,17 @@
+/**
+ * @file Mongoose model for dog adoption statistics by location.
+ * @module src/models/Location.js
+ * @author Hanna Rubio Vretby <hr222sy@student.lnu.se>
+ * @version 1.0.0
+ */
+
 import mongoose from 'mongoose'
 
 const locationSchema = new mongoose.Schema({
-  location: { type: String, unique: true },
+  location: { type: String, required: true, unique: true },
   exported: { type: Number },
   imported: { type: Number },
-  totalInState: { type: Number },
+  totalInState: { type: Number }
 }, { timestamps: true })
 
 export default mongoose.model('Location', locationSchema)
