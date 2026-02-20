7471b40d83c49e520c33a05237de931286e54a9f
Hanna Vretby <hr222sy@student.lnu.se>
2026-02-20 15:44:01 +0100

    refactor: add JSDoc header and required to petfinder_id in Travel model

--- src/models/Travel.js
+++ src/models/Travel.js
@@ -1,7 +1,14 @@
+/**
+ * @file Mongoose model for dog travel records.
+ * @module src/models/Travel.js
+ * @author Hanna Rubio Vretby <hr222sy@student.lnu.se>
+ * @version 1.0.0
+ */
+
 import mongoose from 'mongoose'
 
 const travelSchema = new mongoose.Schema({
-  petfinder_id: { type: String },
+  petfinder_id: { type: String, required: true },
   contact_state: { type: String },
   found: { type: Date },
   still_there: { type: Boolean }
