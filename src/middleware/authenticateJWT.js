8d9b3bca974f7d840ae0eaa1a900f70dce41352c
Hanna Vretby <hr222sy@student.lnu.se>
2026-02-20 15:40:20 +0100

    refactor: add JSDoc header and improve readability in authenticateJWT

--- src/middleware/authenticateJWT.js
+++ src/middleware/authenticateJWT.js
@@ -1,9 +1,16 @@
+/**
+ * @file JWT authentication middleware.
+ * @module src/middleware/authenticateJWT.js
+ * @author Hanna Rubio Vretby <hr222sy@student.lnu.se>
+ * @version 1.0.0
+ */
+
 import jwt from 'jsonwebtoken'
 import UnauthorizedError from '../utils/errors/UnauthorizedError.js'
 
 const verifyToken = (authorizationHeader) => {
   const headerIsMissing = !authorizationHeader
-  const headerDoesNotStartWithBearer = !authorizationHeader?.startsWith('Bearer ')
+  const headerDoesNotStartWithBearer = !authorizationHeader.startsWith('Bearer ')
 
   if (headerIsMissing || headerDoesNotStartWithBearer) {
     throw new UnauthorizedError('No token provided')
