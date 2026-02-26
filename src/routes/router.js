153fb13c7b24d1cd7e54963bf3fec7fbc4e72a9a
Hanna Vretby <hr222sy@student.lnu.se>
2026-02-26 22:22:46 +0100

    update: router.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,19 +1,19 @@
 /**
- * @file Main router.
+ * @file Main application router.
  * @module src/routes/router.js
  * @author Hanna Rubio Vretby <hr222sy@student.lnu.se>
  * @version 1.0.0
  */
 
 import express from 'express'
-import dogRouter from './DogRouter.js'
-import travelRouter from './TravelRouter.js'
-import locationRouter from './LocationRouter.js'
-import authenticationRouter from './AuthenticationRouter.js'
+import DogRouter from './DogRouter.js'
+import TravelRouter from './TravelRouter.js'
+import LocationRouter from './LocationRouter.js'
+import AuthenticationRouter from './AuthenticationRouter.js'
 
 export const router = express.Router()
 
-router.use('/dogs', dogRouter)
-router.use('/travel', travelRouter)
-router.use('/locations', locationRouter)
-router.use('/auth', authenticationRouter)
+router.use('/dogs', new DogRouter().getRouter())
+router.use('/travel', new TravelRouter().getRouter())
+router.use('/locations', new LocationRouter().getRouter())
+router.use('/auth', new AuthenticationRouter().getRouter())
