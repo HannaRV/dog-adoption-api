e2a65fe9e3e50210804326b15a7cb4d90df6f76a
Hanna Vretby <hr222sy@student.lnu.se>
2026-03-03 20:37:12 +0100

    feat: add locationLoader

---
+++ src/utils/seeds/loaders/locationLoader.js
@@ -0,0 +1,38 @@
+/**
+ * @file Loader for location documents.
+ * @module src/utils/seed/loaders/locationLoader.js
+ * @author Hanna Rubio Vretby <hr222sy@student.lnu.se>
+ * @version 1.0.0
+ */
+
+import LocationRepository from '../../../repositories/LocationRepository.js'
+
+/**
+ * Loads transformed location data into the database.
+ */
+export default class LocationLoader {
+  #locationRepository
+
+  /**
+   * @param {LocationRepository} [locationRepository] - Injected for testing.
+   */
+  constructor (locationRepository = new LocationRepository()) {
+    this.#locationRepository = locationRepository
+  }
+
+  /**
+   * Inserts a batch of location documents.
+   *
+   * @param {object[]} locations - Transformed location documents.
+   */
+  async load (locations) {
+    await this.#locationRepository.insertMany(locations)
+  }
+
+  /**
+   * Removes all location documents.
+   */
+  async clear () {
+    await this.#locationRepository.deleteAll()
+  }
+}
