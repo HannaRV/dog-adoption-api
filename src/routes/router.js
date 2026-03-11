d99a544357c92e9b534a27172188933d611a5b26
Hanna Vretby <hr222sy@student.lnu.se>
2026-03-11 21:04:25 +0100

    feat: add welcome route GET /api/v1/

--- src/routes/router.js
+++ src/routes/router.js
@@ -11,9 +11,20 @@ import TravelRouter from './TravelRouter.js'
 import LocationRouter from './LocationRouter.js'
 import AuthenticationRouter from './AuthenticationRouter.js'
 import { NotFoundError } from '../utils/errors/NotFoundError.js'
+import { HTTP_STATUS } from '../config/httpStatus.js'
 
 export const router = express.Router()
 
+router.get('/', (req, res) => {
+  res.status(HTTP_STATUS.OK).json({
+    message: 'Welcome to the Dog Adoption API v1!',
+    resources: {
+      dogs: `${req.protocol}://${req.get('host')}/api/v1/dogs`,
+      travel: `${req.protocol}://${req.get('host')}/api/v1/travel`,
+      locations: `${req.protocol}://${req.get('host')}/api/v1/locations`
+    }
+  })
+})
 router.use('/dogs', new DogRouter().getRouter())
 router.use('/travel', new TravelRouter().getRouter())
 router.use('/locations', new LocationRouter().getRouter())
