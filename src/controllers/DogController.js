5c3fca923e62f660d531da9c7cc82a3cd011cfc5
Hanna Vretby <hr222sy@student.lnu.se>
2026-04-15 15:25:12 +0200

    feat: add contactState filter, puerto rico location, and update continental filter

--- src/controllers/DogController.js
+++ src/controllers/DogController.js
@@ -122,15 +122,17 @@ export default class DogController {
     }
   }
 
-  #buildFilter ({ breedPrimary, age, sex, size, location, envChildren, envDogs, envCats, houseTrained }) {
+  #buildFilter ({ breedPrimary, age, sex, size, location, contactState, envChildren, envDogs, envCats, houseTrained }) {
     const filter = {}
     if (breedPrimary) filter.breed_primary = breedPrimary
     if (age) filter.age = age
     if (sex) filter.sex = sex
     if (size) filter.size = size
-    if (location === 'continental') filter.contact_state = { $nin: ['HI', 'AK'] }
+    if (contactState && !location) filter.contact_state = contactState
+    if (location === 'continental') filter.contact_state = { $nin: ['HI', 'AK', 'PR'] }
     else if (location === 'hawaii') filter.contact_state = 'HI'
     else if (location === 'alaska') filter.contact_state = 'AK'
+    else if (location === 'puerto_rico') filter.contact_state = 'PR'
     if (envChildren === 'true') filter.env_children = true
     if (envDogs === 'true') filter.env_dogs = true
     if (envCats === 'true') filter.env_cats = true
